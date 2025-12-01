9f34ec6e61c6bf1047380f188c5aadc69610823a
Vedant Pankar <vedantpankar2@gmail.com>
2025-12-01 23:32:40 +0530

    first push to production

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/api/message`)
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching message", error));
+  }, []);
+
+  return (
+    <>
+      <h1>Welcome to chai aur code frontend</h1>
+      <h2>Data: {message}</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:3000",
+      // add production url
+    ],
+    credentials: true,
+  })
+);
+
+app.get("/api/message", (req, res) => {
+  res.json({
+    message: "Hello from chai aur code server",
+  });
+});
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
